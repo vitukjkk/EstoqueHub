9276223f4a16ba216337756e8ccf51b3e5753467
Vitor Colombelli <vittorcolom@gmail.com>
2025-01-09 15:42:55 -0300

    feat: terminando de criar componentes header e footer

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Header from "@/app/components/Header/header"
+import Footer from "@/app/components/Footer/footer"
+
+export default function Home() {
+    return (
+        <div>
+            <Header/>
+            ola mundo!
+            <Footer/>
+        </div>
+    )
+}
